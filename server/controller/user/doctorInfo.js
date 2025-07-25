a15e45687199980b14eeac52619519a8ef284a65
swayam agarwal <you@example.com>
2025-07-25 20:51:53 +0530

    refactored the controllers

--- server/controller/user/doctorInfo.js
+++ server/controller/user/doctorInfo.js
@@ -1,18 +1,13 @@
 import doctormodel from '../../models/user/doctorSchema.js';
+import { asyncHandler } from "../../utils/asyncHandler.js"; // Corrected import
 
-const doctorinfo = async (req, res) => {
-  try {
-    // Fetch all doctors from the database
-    const doctorData = await doctormodel.find({});
+const doctorinfo = asyncHandler(async (req, res, next) => {
+  // Fetch all doctors from the database. 
+  // If this database query fails, asyncHandler will catch the error.
+  const doctorData = await doctormodel.find({});
 
-
-    // Send a success response to the client
-    res.status(200).json({ success: true, data: doctorData });
-  } catch (error) {
-    // Log the error and send an error response
-    console.error('Error fetching doctor data:', error);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
-};
+  // Send a success response to the client
+  res.status(200).json({ success: true, data: doctorData });
+});
 
 export default doctorinfo;
