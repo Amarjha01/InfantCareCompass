cc67712d7c02c61bc2c511a64a1081b42cd999a5
gouravKJ <gouravjaiswal2006@gmail.com>
2025-07-30 00:21:22 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -31,7 +31,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
 app.use("/api/github", githubWebhook);
-
+//error handling 
+app.use((req,res,next)=>{
+  res.status(404).json({message:'Route Not Found'});
+});
+//500 error handling
+app.use((err,req,res,next)=>{
+  console.error(err.stack);
+  res.status(500).json({message:'internal server error'});
+});
 // Database connection and server start
 dbConnect().then(() => {
   app.listen(PORT, () => {
