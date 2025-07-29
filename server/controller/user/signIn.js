2f9c7a03bef649a8553c88a7a82477e9bec9d5fc
swayam agarwal <you@example.com>
2025-07-29 20:35:37 +0530

    refactored the controller logic and removed issues

--- server/controller/user/signIn.js
+++ server/controller/user/signIn.js
@@ -1,82 +1,42 @@
-import { asyncHandler } from "../../utils/asyncHandler.js"; // Import the handler
+import { asyncHandler } from "../../utils/asyncHandler.js";
 import usermodel from "../../models/user/user.js";
 import doctormondel from "../../models/user/doctorSchema.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-const signin = asyncHandler(async (req, res, next) => {
-    // The original try...catch block is removed, but the inner logic is identical.
-    const { email, password, role } = req.body;
+const signin = asyncHandler(async (req, res) => {
+  const { email, password, role } = req.body;
 
-    if (role === 'DOCTOR') {
-        const doctor = await doctormondel.findOne({ email });
-        if (!doctor) {
-            req.status(400).json({
-                message: 'user not found'
-            })
-        }
+  const modelToUse = role === 'DOCTOR' ? doctormondel : usermodel;
+  const user = await modelToUse.findOne({ email });
 
-        const verfyuser = await bcrypt.compare(password, doctor.password);
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
 
-        if (verfyuser) {
-            const tokendata = {
-                id: doctor._id,
-                email: doctor.email
-            }
-            
-            const token = jwt.sign(tokendata, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
-            // const refreshtoken =  jwt.sign({tokendata}, process.env.TOKEN_SECRET_KEY, {expiresIn:"7d"});
-            const tokenOption = {
-                httpOnly: true,
-                secure: true
-            }
-            res.cookie("token", token, tokenOption).status(200).json({
-                message: "login sucessfully",
-                data: doctor,
-                success: true,
-                error: false
-            })
-            navigate("/")
-            // res.status(200).json({doctor
-            // })
-        } else {
-            res.status(400).json({
-                message: "please enter password correctly"
-            })
-        }
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect password" });
+  }
 
-    } else {
-        const user = await usermodel.findOne({ email });
-        if (!user) {
-            req.status(400).json({
-                message: 'user not found'
-            })
-        }
-        const veryfyuser = await bcrypt.compare(password, user.password);
+  const tokendata = {
+    id: user._id,
+    email: user.email
+  };
 
-        if (veryfyuser) {
-            const tokendata = {
-                id: user._id,
-                email: user.email
-            }
-            const token = jwt.sign(tokendata, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
-            // const refreshtoken =  jwt.sign({tokendata}, process.env.TOKEN_SECRET_KEY, {expiresIn:"7d"});
-            const tokenOption = {
-                httpOnly: true,
-                secure: true
-            }
-            res.cookie("token", token, tokenOption).status(200).json({
-                message: "login sucessfully",
-                data: user,
-                success: true,
-                error: false
-            })
-        } else {
-            res.status(400).json({
-                message: "please enter password correctly"
-            })
-        }
-    }
+  const token = jwt.sign(tokendata, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
+
+  const tokenOptions = {
+    httpOnly: true,
+    secure: true
+  };
+
+  res.cookie("token", token, tokenOptions).status(200).json({
+    message: "Login successfully",
+    data: user,
+    success: true,
+    error: false
+  });
 });
 
 export default signin;
