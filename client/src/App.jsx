05abf91903537e8a595afeb5f768ccc690fe803a
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2025-07-30 03:57:50 +0530

    Refactor App component and add ScrollRestoration for improved navigation experience

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,34 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import { Outlet,useLocation,useMatches } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import { Outlet, useMatches } from "react-router-dom";
+// import { Toaster } from "react-hot-toast";
 
+import FloatingScrollToTop from "./components/ScrollToTop";
+// import CustomCursor from "./components/CustomCursor";
 
-import FloatingScrollToTop from './components/ScrollToTop';
-import CustomCursor from './components/CustomCursor';
-
-import ScrollToTop from './components/ScrollToTop';
-
+import ScrollToTop from "./components/ScrollToTop";
+import ScrollRestoration from "./components/ScrollRestoration";
 
 function App() {
   const matches = useMatches();
   const hideLayout = matches.some((match) => match.handle?.noLayout);
 
-  return (
 
+  return (
     <div className="w-full">
+      <ScrollRestoration />
       {!hideLayout && <Header />}
 
       <ScrollToTop />
-      
+
       <main className="mt-24">
         <Outlet />
       </main>
 
       {!hideLayout && <Footer />}
-       <FloatingScrollToTop />
+      <FloatingScrollToTop />
     </div>
   );
 }
