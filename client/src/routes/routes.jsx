6808f296b970180d00fef07c4355413e34e3b122
Bindu2006 <svbsathwika2006@gmail.com>
2025-07-30 12:38:39 +0530

    Added Copilot assistant #145

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -12,6 +12,7 @@ import ConsultationPage from "../pages/consult.jsx";
 import VideoRoom from "../pages/VideoRoom.jsx";
 import PersonalizedLearningHub from "../pages/LearningHub.jsx";
 import VaccineReminder from "../pages/VaccineReminder.jsx";
+import CareCoPilot from "../pages/CareCoPilot.jsx";
 import NotFoundPage from "../pages/NotFoundPage.jsx";
 import Contributors from "../pages/Contributors";
 
@@ -62,6 +63,10 @@ const router = createBrowserRouter([
       {
         path: "vaccineReminder",
         element: <VaccineReminder />,
+      },
+      {
+        path: "care-co-pilot",
+        element: <CareCoPilot />,
       },
     
 
