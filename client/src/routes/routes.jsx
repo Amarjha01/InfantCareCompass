dc7ef7b9e232365c601dc503c5fd02dd11f99d85
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-08-02 19:02:28 +0530

    fixed footer alignment

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,21 +1,21 @@
 import App from "../App";
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from "../pages/Home";
-import About  from  "../pages/About";
-import Blog  from  "../pages/Blog";
-import ContactUs  from  "../pages/ContactUs";
-import News from  "../pages/News";
-import Contributors  from  "../pages/Contributors";
-import VideoRoom from  "../pages/VideoRoom";
-import Signin from  "../pages/SignIn";
-import Registration from  "../pages/Registration";
-import NotFoundPage from  "../pages/NotFoundPage";
-import CareCoPilot from  "../pages/CareCoPilot";
-import VaccineReminder from  "../pages/VaccineReminder";
-import PersonalisedLearningHub from  "../pages/LearningHub";
-import DoctorDetails from  "../pages/DoctorDetails";
+import About from "../pages/About";
+import Blog from "../pages/Blog";
+import ContactUs from "../pages/ContactUs";
+import News from "../pages/News";
+import Contributors from "../pages/Contributors";
+import VideoRoom from "../pages/VideoRoom";
+import Signin from "../pages/SignIn";
+import Registration from "../pages/Registration";
+import NotFoundPage from "../pages/NotFoundPage";
+import CareCoPilot from "../pages/CareCoPilot";
+import VaccineReminder from "../pages/VaccineReminder";
+import PersonalisedLearningHub from "../pages/LearningHub";
+import DoctorDetails from "../pages/DoctorDetails";
 import LearningHub from "../pages/LearningHub";
-import consultationPage from  "../pages/consult";
+import consultationPage from "../pages/consult";
 
 const router = createBrowserRouter([
   {
@@ -64,16 +64,15 @@ const router = createBrowserRouter([
         path: "vaccineReminder",
         element: <VaccineReminder />,
       },
+
       {
-{
-  path: "care-co-pilot",
-  element: <CareCoPilot />,
-},
-{
-  path: "*",
-  element: <NotFoundPage />,
-  handle: { noLayout: true },
-}
+        path: "care-co-pilot",
+        element: <CareCoPilot />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+        handle: { noLayout: true },
 
       },
     ],
